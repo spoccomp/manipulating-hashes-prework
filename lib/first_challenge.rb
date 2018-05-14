40ae23460a7799bfa8939a609eef8909ddd4b3c6
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-14 02:36:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       contacts.delete_if {|attribute, value| value == "strawberry" }
-      
+
     end
   end
   #remember to return your newly altered contacts hash!
